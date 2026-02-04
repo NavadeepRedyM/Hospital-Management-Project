9a8ba68661d26b86c6197e8f098d196d1a614d94
Bhavani Ganapathi <gangumalla-lakshmi.bhavani-ganapathi@capgemini.com>
2026-02-04 09:16:47 +0530

    view appointments

---
+++ 10-springboot-HospitalManagement/src/main/java/com/cg/controller/AppointmentController.java
@@ -0,0 +1,29 @@
+package com.cg.controller;
+ 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+ 
+import com.cg.service.AppointmentService;
+ 
+@Controller
+@RequestMapping("/appointments")
+public class AppointmentController {
+ 
+    @Autowired
+    private AppointmentService appointmentService;
+ 
+    // ONLY VIEW LIST
+    @GetMapping
+    public String viewAppointments(Model model) {
+        model.addAttribute(
+                "appointments",
+                appointmentService.getAllAppointments()
+        );
+        return "hospital/view-appointments";
+    }
+    
+}
+ 
