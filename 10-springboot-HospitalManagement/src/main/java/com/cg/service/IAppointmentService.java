809485cbd1825d6df5da8f4c37f69863fea044ca
Mary Prathima <mary-prathima.mary-prathima@capgemini.com>
2026-02-05 09:26:49 +0530

    view patients,patient dashboard

--- 10-springboot-HospitalManagement/src/main/java/com/cg/service/IAppointmentService.java
+++ 10-springboot-HospitalManagement/src/main/java/com/cg/service/IAppointmentService.java
@@ -1,13 +1,19 @@
 package com.cg.service;
- 
-import java.util.List;
+
 import com.cg.model.Appointment;
- 
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+
 public interface IAppointmentService {
- 
+
+	Appointment book(String username, Long departmentId, Long doctorId, LocalDate date, String time, String reason);
+    List<Appointment> listForPatient(Long patientId);
     List<Appointment> getAllAppointments();
- 
+    
+    // Add these so the implementation class doesn't fail
     Appointment getAppointmentById(Long id);
- 
     void deleteAppointment(Long id);
 }
